bc08e2de8ff42b1033cc7ec23a5ee98e635299c5
Rob Wright <bobazure1969@gmail.com>
2023-07-11 07:37:14 +0100

    initial commit

---
+++ Database1/dbo/Tables/Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [StudentId]        UNIQUEIDENTIFIER NOT NULL,
+    [Name]             NVARCHAR (MAX)   NULL,
+    [Age]              INT              NOT NULL,
+    [IsRegularStudent] BIT              DEFAULT (CONVERT([bit],(1))) NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([StudentId] ASC)
+);
+
